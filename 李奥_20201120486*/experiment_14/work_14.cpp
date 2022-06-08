ea4aa085fd8ac9f5454db1cea05781317d380a0a
SuperLeo2021 <1430428252@qq.com>
2022-06-08 02:10:07 +0000

    add

--- 李奥_20201120486*/experiment_14/work_14.cpp
+++ 李奥_20201120486*/experiment_14/work_14.cpp
@@ -1,89 +1,98 @@
-#include<GL/glut.h>
-#include<stdlib.h>
-#include<math.h>
-
-GLsizei winWidth = 600, winHeight = 600;
-GLfloat xwcMin = -50.0, xwcMax = 50.0;
-GLfloat ywcMin = -50.0, ywcMax = -50.0;
-class wcPt3D {
-public:
-	GLfloat x, y, z;
+#include <GL/glut.h>
+#include <stdio.h>
+#include <math.h>
+GLsizei winWIdth = 600, winHeight = 600;
+GLfloat xwcMin = -50.0, xwcMax = 50.0, ywcMin = -50.0, ywcMax = 50.0;
+class wcPt3D
+{
+public: GLfloat x, y, z;
 };
 void init(void)
 {
-	glClearColor(1.0,1.0,1.0,1.0);
-
+    glClearColor(1.0, 1.0, 1.0, 0.0);
 }
-void plotPoint(wcPt3D bezCurvePt) {
-	glBegin(GL_POINTS);
-	glVertex2f(bezCurvePt.x, bezCurvePt.y);
-	glEnd();
+void plotPoint(wcPt3D p)
+{
+    glBegin(GL_POINTS);
+    glVertex2f(p.x, p.y);
+    glEnd();
 }
 void binomialCoeffs(GLint n, GLint* C)
 {
-	GLint k, j;
-	for (k = 0; k <= n; k++) {
-		C[k] = 1;
-		for (j = n; j <= k + 1; j--)
-			C[k] *= j;
-		for (j = n - k; j >= 2; j--)
-			C[k] /= j;
-	}
+    GLint i, j;
+    for (i = 0; i <= n; i++)
+    {
+        C[i] = 1;
+        for (j = n; j >= i + 1; j--)
+        {
+            C[i] *= j;
+        }
+        for (j = n - i; j >= 2; j--)
+        {
+            C[i] /= j;
+        }
+    }
 }
-void computeBezpt(GLfloat u, wcPt3D* bezPt, GLint nCtrlPts, wcPt3D* ctrlPts, GLint* C) {
-	GLint k, n = nCtrlPts - 1;
-	GLfloat bezBelendFcn;
-	bezPt->x = bezPt->y = bezPt->z = 0.0;
-	for (k = 0; k <= nCtrlPts; k++) {
-		bezBelendFcn = C[k] * pow(u, k) * pow(1 - u, n - k);
-		bezPt->x+= ctrlPts[k].x * bezBelendFcn;
-		bezPt->y += ctrlPts[k].y * bezBelendFcn;
-		bezPt->z+= ctrlPts[k].z * bezBelendFcn;
-	}
-}
-void bezier(wcPt3D* ctrlPts, GLint nCtrlPts, GLint nBezCurvePts)
+void computeBezPt(GLfloat u, wcPt3D* bezPt, GLint nCtrlPts, wcPt3D* ctrlPts, GLint* C)
 {
-	wcPt3D bezCurvept;
-	GLfloat u;
-	GLint* C, k;
-	C = new GLint[nCtrlPts];
-	binomialCoeffs(nCtrlPts - 1, C);
-	for (k = 0; k <= nBezCurvePts; k++) {
-		u = GLfloat(k) / GLfloat(nBezCurvePts);
-		computeBezpt(u, &bezCurvept, nCtrlPts, ctrlPts, C);
-		plotPoint(bezCurvept);
-	}
-	delete[] C;
+    GLint i, j = nCtrlPts - 1;
+    GLfloat bezBlendFcn;
+    bezPt->x = bezPt->y = bezPt->z = 0.0;
+    for (i = 0; i < nCtrlPts; i++)
+    {
+        bezBlendFcn = C[i] * pow(u, j) * pow(1 - u, j - i);
+        bezPt->x += ctrlPts[i].x * bezBlendFcn;
+        bezPt->y += ctrlPts[i].y * bezBlendFcn;
+        bezPt->z += ctrlPts[i].z * bezBlendFcn;
+
+    }
+}
+void bezier(wcPt3D* ctrlPts, GLint nCtrlPts, GLint nBezPts)
+{
+    GLint* C, k;
+    GLfloat u;
+    wcPt3D bezPt;
+    C = new GLint[nCtrlPts];
+    binomialCoeffs(nCtrlPts - 1, C);
+    for (k = 0; k <= nBezPts; k++)
+    {
+        u = GLfloat(k) / GLfloat(nBezPts);
+        computeBezPt(u, &bezPt, nCtrlPts, ctrlPts, C);
+        plotPoint(bezPt);
+    }
+    delete[] C;
 }
 void displayFcn(void)
 {
-	GLint nCtrlpts = 4, nBezCurvePts = 1000;
-	wcPt3D ctrlPts[4] = { {-40.0,-40.0,0.0},{-10.0,200.0,0.0},{10.0,-200.0,0.0},{40.0,40.0,0.0} };
-	glClear(GL_COLOR_BUFFER_BIT);
-	glPointSize(4);
-	glColor3f(1.0, 0.0, 0.0);
-	bezier(ctrlPts, nCtrlpts, nBezCurvePts);
-	glFlush();
+    GLint nCtrlPts = 4, nBezCurvePts = 1000;
+    wcPt3D ctrlPts[4] = {
+        {-40.0, -40.0, 0.0},
+        {-10.0, 200.0, 0.0},
+        {10.0, -200.0, 0.0},
+        {40.0, 40.0, 0.0} };
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPointSize(4);
+    glColor3f(0.0,1.0,1.0);
+    bezier(ctrlPts, nCtrlPts, nBezCurvePts);
+    glFlush();
 }
 void winReshapeFcn(GLint newWidth, GLint newHeight)
 {
-	glViewport(0, 0, newHeight,  newHeight);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(xwcMin, xwcMax, ywcMin, ywcMax);
-	glClear(GL_COLOR_BUFFER_BIT);
+    glViewport(0, 0, newWidth, newHeight);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(xwcMin, xwcMax, ywcMin, ywcMax);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
-int main(int argc, char** argv)
+void main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(50, 50);
-	glutInitWindowSize(winWidth, winHeight);
-	glutCreateWindow("bezier 曲线");
-
-	init();
-	glutDisplayFunc(displayFcn);
-	glutReshapeFunc(winReshapeFcn);
-
-	glutMainLoop();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(50,70);
+    glutInitWindowSize(winWIdth, winHeight);
+    glutCreateWindow("Bezier曲线");
+    init();
+    glutDisplayFunc(displayFcn);
+    glutReshapeFunc(winReshapeFcn);
+    glutMainLoop();
 }
